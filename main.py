5dfe69d596b97ae2828b45f57c526be207229c9a
rakshyakOpscale <rakshyak@opscale.io>
2023-05-04 11:40:16 +0530

    init commit

---
+++ main.py
@@ -0,0 +1,38 @@
+import torch
+from diffusers import StableDiffusionPipeline
+
+from flask import Flask, request, render_template
+from flask_ngrok import run_with_ngrok
+
+import base64
+from io import BytesIO
+
+app = Flask(__name__)
+run_with_ngrok(app)
+
+pipe = StableDiffusionPipeline.from_pretrained("runwayml/stable-diffusion-v1-5", revision="fp16", torch_dtype=torch.float16)
+pipe.to('cuda')
+
+@app.route('/')
+def initial():
+    return render_template('index.html')
+
+@app.route('/submit-prompt', methods=['POST'])
+def generate_image():
+    prompt = request.form['prompt-input']
+    print(f'Generating an image of {prompt}')
+
+    image = pipe(prompt).images[0]
+    print('Image generated! Converting image...')
+
+    buffered = BytesIO
+    image.save(buffered, format='PNG')
+    img_str = base64.b32encode(buffered.getvalue())
+    img_str = 'data:image/png;base64,' + str(img_str)[2:-1]
+
+    print('Sending image...')
+    return render_template('index.html', generate_image=img_str)
+
+
+if __name__ == '__main__':
+    app.run()
